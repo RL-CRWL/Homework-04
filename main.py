e67807590bf65e7099ce822c745b9d0af5abe142
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-09-02 21:36:56 +0200

    Update and rename deepseek-main.py to main.py
    Added contributors

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Contributors
+# 2602515   Taboka Dube
+# 2541693   Wendy Maboa
+# 2596852   Liam Brady
+# 2333776   Refiloe Mopeloa
+
+
 import numpy as np
 import gymnasium as gym
 import matplotlib.pyplot as plt
