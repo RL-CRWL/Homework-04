f4a04cc4f6c7303dce05e205b868958dc8c7fdf3
Limes <LMBrady5454@gmail.com>
2025-09-02 18:50:59 +0200

    added names

--- Homework4.py
+++ Homework4.py
@@ -1,3 +1,9 @@
+# Contributors
+# 2602515   Taboka Dube
+# 2541693   Wendy Maboa
+# 2596852   Liam Brady
+# 2333776   Refiloe Mopeloa
+
 import gymnasium as gym
 from collections import defaultdict
 import numpy as np
